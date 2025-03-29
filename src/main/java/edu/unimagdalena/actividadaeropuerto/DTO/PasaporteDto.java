9aa65cadd6945d9df2afa5ee4f8bc24809fed431
Anderson Bornachera <andersonb001b@gmail.com>
2025-03-28 19:21:16 -0500

    Creación de PasaporteDto

---
+++ src/main/java/edu/unimagdalena/actividadaeropuerto/DTO/PasaporteDto.java
@@ -0,0 +1,11 @@
+package edu.unimagdalena.actividadaeropuerto.DTO;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+
+public class PasaporteDto {
+    private final String numero;
+}
